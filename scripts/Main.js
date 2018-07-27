e934d84e017611832f10d7e824a37d9be1ad31a3
Jeff J. Taylor-Chang <jeff@jefftc.com>
2018-07-27 08:50:27 -0400

    Started setting up initial server

---
+++ scripts/Main.js
@@ -0,0 +1,27 @@
+/**
+ * Load the contents of a given JSON file
+ *
+ * @param {function (string)} callback the callback function that receives the response
+ */
+function loadFile(sourceUrl, callback) {
+  console.log("Loading file asynchronous", sourceUrl);
+
+  var xobj = new XMLHttpRequest();
+
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", sourceUrl, true); // currently asynchronous (async|sync)
+  xobj.onreadystatechange = function() {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      console.log("XMLHttp Response received", xobj);
+      callback(xobj.responseText);
+    }
+  };
+
+  xobj.send(null);
+}
+
+function run() {
+  loadFile("../training/testlabels", function(data) {
+    console.log(data);
+  });
+}
